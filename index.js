a8abfca669d11a236b1b5c7d19da43848cce3107
piered <52042352+piered@users.noreply.github.com>
2025-05-09 14:50:09 +0200

    Corso js

--- index.js
+++ index.js
@@ -41,3 +41,17 @@ let person={
 };
 //              Array
 let elenco=[1,2,3,4,5];
+
+
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
