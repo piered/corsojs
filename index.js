21808f3256e7fef5dc18e75e4fa3f805927a878d
piered <52042352+piered@users.noreply.github.com>
2025-05-09 14:34:54 +0200

    Corso js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log('Happy developing ✨')
+
+function example() {
+    if (true) {
+        var x = 10; // var function level scope
+    }
+    console.log(x);
+}
+
+example();
+//console.log(x); // da errore perchè esterno alla funzione
+
+/*
+    let block level scope
+ */
+{
+    let y = 20;
+    console.log(y);
+}
+//console.log(y); // da errore perchè esterno al blocco
+
+const autore = "Pinco pallino";
+console.log(autore);
+   // const block level scope
+
+//          PRIMITIVE TYPE
+let name='Alice';       //  TYPE String
+let age=2;            //    TYPE NUMBER
+let price=2.3;          // TYPE NUMBER
+let isStudent=false;    // TYPE BOOLEAN
+let x1;             // undefined DECLARED BUT NOT ASSIGNED A VALUE
+let y1=null;    // NULL EMPTY VALUE
+
+//           COMPOSITE DATA TYPES
+//              Objects
+let person={
+    name:'Alice',
+    age:2,
+    price:2.3,
+    isStudent:false
+};
+//              Array
+let elenco=[1,2,3,4,5];
